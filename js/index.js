d09fb92b6ccf6d9aa64bd04cce9e69c845b84ca1
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-29 15:38:55 -0700

    Fun in the Sun moduals reverse colors.

--- js/index.js
+++ js/index.js
@@ -14,3 +14,16 @@ button.forEach(button => {
         e.target.style.cssText = "background-color: #17A2B8";
     });
 });
+
+// Fun in the sun reverse colors on mouse enter on each module. Then go back when the mouse leaves.
+const fun = document.querySelectorAll('.destination');
+fun.forEach(fun => {
+    fun.addEventListener('mouseenter', e => {
+        e.target.style.cssText = "color: white; background-color: black";
+    });
+});
+fun.forEach(fun => {
+    fun.addEventListener('mouseleave', e => {
+        e.target.style.cssText = "color: black; background-color: white";
+    });
+});
