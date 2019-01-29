4b3883dc57ae3fee3c7512d6bc307a5cfd88a9c7
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-29 15:06:00 -0700

    Buttons go back to original color when clicked.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@
 const button = document.querySelectorAll('.btn');
 button.forEach(button => {
     button.addEventListener('mouseover', e => {
-        event.target.style['background-color'] = 'crimson';
+        e.target.style['background-color'] = 'crimson';
+    });
+});
+
+// change back to normal color on click
+button.forEach(button => {
+    button.addEventListener('click', e => {
+        e.target.style.cssText = "background-color: #17A2B8";
     });
 });
