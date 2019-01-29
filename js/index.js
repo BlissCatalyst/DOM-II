d6981fee85b47c2556c035b57199542545f612ce
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-29 14:56:33 -0700

    Change button background color on mouseover.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// change button color to crimson on mouseover.
+const button = document.querySelectorAll('.btn');
+button.forEach(button => {
+    button.addEventListener('mouseover', e => {
+        event.target.style['background-color'] = 'crimson';
+    });
+});
